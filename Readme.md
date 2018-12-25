2b79e9217b08ca8ffee3983b31a20ef1e39c7728
Титаренко Александр Викторович <alexander.titarenko@resmi.kz>
2018-12-25 12:22:39 +0600

    Minor updates
    Updated Readme
    Added Override function
    
    Signed-off-by: Титаренко Александр Викторович <alexander.titarenko@resmi.kz>

--- Readme.md
+++ Readme.md
@@ -2,9 +2,16 @@
 This package was developed to improve the workflow when using the environmental variables.
 
 ### Use case
-1) When you want to use default value for listening port but want the value to be overwritable by ENV
-2) using localhost for database connection but overwritable in prod by ENV
+1) When you want to use default value for listening port but want the value to be overridable by ENV
+2) Using localhost for database connection but overridable in prod by ENV
+3) Any other value that u want to be controllable from outside the config files
 
 ### Available methods
 
-Env(string key,default value)string - Get the string equivelent of the `key` or `value` will be returned if the `key` is null or non present
+Env(string key,default value)string - Get the string equivalent of the `key` or `value` will be returned if the `key` is null or non present
+
+EnvI(string key,default value)int - Get the int equivalent of the `key` or `value` will be returned if the `key` is null or non present
+
+EnvB(string key,default value)bool - Get the bool equivalent of the `key` or `value` will be returned if the `key` is null or non present
+
+Override(key string, value interface{}) - Override any values that might be fetched. This is usually great for testing
--- subenv.go
+++ subenv.go
@@ -5,12 +5,31 @@ import (
 	"strconv"
 )
 
+var (
+	override = new(map[string]interface{})
+)
+
+/**
+Override any vars that might be fetched in the future
+*/
+func Override(key string, value interface{}) {
+	(*override)[key] = value
+}
+
 /**
 Get an integer equivalent of environmental variable. Return default value of the value is not found or is empty
- */
+*/
 func EnvI(key string, defaultIfNull int) int {
 	var val string
 	var ok bool
+
+	// Check for any overridden values
+	if val, ok := (*override)[key]; ok {
+		iVal, iOk := val.(int)
+		if iOk {
+			return iVal
+		}
+	}
 
 	if val, ok = os.LookupEnv(key); ok {
 		if len(val) == 0 {
@@ -27,10 +46,18 @@ func EnvI(key string, defaultIfNull int) int {
 
 /**
 Get the string equivalent of environmental variable. Return default value of the value is not found or is empty
- */
+*/
 func Env(key string, defaultIfNull string) string {
 	var val string
 	var ok bool
+
+	// Check for any overridden values
+	if val, ok := (*override)[key]; ok {
+		iVal, iOk := val.(string)
+		if iOk {
+			return iVal
+		}
+	}
 
 	if val, ok = os.LookupEnv(key); ok {
 		if len(val) == 0 {
@@ -45,10 +72,18 @@ func Env(key string, defaultIfNull string) string {
 
 /**
 Get the boolean equivalent of environmental variable. Return default value of the value is not found or is empty
- */
+*/
 func EnvB(key string, defaultIfNull bool) bool {
 	var val string
 	var ok bool
+
+	// Check for any overridden values
+	if val, ok := (*override)[key]; ok {
+		iVal, iOk := val.(bool)
+		if iOk {
+			return iVal
+		}
+	}
 
 	if val, ok = os.LookupEnv(key); ok {
 		if len(val) == 0 {
